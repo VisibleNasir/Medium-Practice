8bd4c06654570a5af19d865c03757abf3ac7ca96
Nasir Nadaf <109416738+VisibleNasir@users.noreply.github.com>
2025-07-08 22:34:04 +0530

    step 1

---
+++ README.md
@@ -0,0 +1,3 @@
+"# practice" 
+"# MediumPracticeProject" 
+"# MediumPracticeProject" 
---
+++ backend/src/index.ts
@@ -0,0 +1,79 @@
+import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/extension'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import {sign, verify } from 'hono/jwt'
+
+const app = new Hono()
+app.get('/', (c) => {
+  return c.text('Home page!')
+})
+app.use('/api/v1/blog/*', async (c, next)=>{
+    const header = c.req.header("Authorization") || "";
+    const token = header.split(" ")[1]
+
+    // @ts-ignore
+    const response = await verify(header , c.env.JWT_SECRET)
+    if(response.id){
+      next()
+    }else{
+      c.status(403);
+      return c.json({error : "unauthorized"});
+    }
+})
+app.post('/api/v1/signup',async (c) => { 
+  const prisma = new PrismaClient({   
+    // @ts-ignore  
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+  
+  const body = await c.req.json();
+
+  const user = await prisma.user.create({
+    data:{
+      email: body.email,
+      password:body.password
+    }
+  })
+  //@ts-ignore
+  const token  = await sign({id: user.id},c.env.JWT_SECRET)
+  return c.json({
+    jwt:token
+  })
+})
+
+app.post('/api/v1/signin', async (c) => {
+  const prisma = new PrismaClient({   
+    // @ts-ignore  
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+
+  const body = await c.req.json();
+  const user = await prisma.user.findUnique({
+    where: {
+      email : body.email,
+      password: body.password
+    }
+  })
+
+  if(!user){
+    c.status(403);
+    return c.json({error : "user not found"});
+  }
+  // @ts-ignore
+  const jwt = await sign({id : user.id} , c.env.JWT_SECRET);
+  return c.json({ jwt });
+})
+
+app.post('/api/v1/blog', (c) => {
+  return c.text('Blog Post!')
+})
+
+app.put('/api/v1/blog', (c) => {
+  return c.text('Blog Put!')
+})
+
+app.get('/api/v1/blog/:id', (c) => {
+  return c.text('Blog Get!')
+})
+
+export default app
